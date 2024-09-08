180663de9520fc08c5a57f23e68051b209990ace
Mike Locks <mikeloc@hotmail.fr>
2024-09-08 17:34:50 +0200

    creation dossiers

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Book = require('./models/book');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://user1:user1@cluster0.rkuf9.mongodb.net/',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+module.exports = app;
